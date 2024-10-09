f246c9f0dbbb24028f1cdc69cee235a9b2db5a3a
Rodrigo Mendes <rodrigom.souza1999@gmail.com>
2024-10-08 23:01:37 -0300

    added ROC curve

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import pandas as pd # manipulação de dados em formato de dataframe
-import numpy as np # operações matemáticas
-import seaborn as sns # visualização gráfica
-import matplotlib.pyplot as plt # visualização gráfica
-from scipy.interpolate import UnivariateSpline # curva sigmoide suavizada
-import statsmodels.api as sm # estimação de modelos
-import statsmodels.formula.api as smf # estimação do modelo logístico binário
-from statstests.process import stepwise # procedimento Stepwise
-from scipy import stats # estatística chi2
-from statsmodels.iolib.summary2 import summary_col # comparação entre modelos
-                                                        #logístico multinomial
-                                                        
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import statsmodels.api as sm 
+import statsmodels.formula.api as smf 
+from statsmodels.iolib.summary2 import summary_col 
+import numpy as np
+import plotly.express as px                                                        
 import warnings
 warnings.filterwarnings('ignore')
+
+# In[0.5]: Construção de função para a definição da matriz de confusão
+
+from sklearn.metrics import confusion_matrix, accuracy_score,\
+    ConfusionMatrixDisplay, recall_score
+
+def matriz_confusao(predicts, observado, cutoff):
+    values = predicts.values
+    
+    predicao_binaria = []
+        
+    for item in values:
+        if item < cutoff:
+            predicao_binaria.append(0)
+        else:
+            predicao_binaria.append(1)  
+           
+    cm = confusion_matrix(predicao_binaria, observado)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.xlabel('True')
+    plt.ylabel('Classified')
+    plt.gca().invert_xaxis()
+    plt.gca().invert_yaxis()
+    plt.show()
+        
+    sensitividade = recall_score(observado, predicao_binaria, pos_label=1)
+    especificidade = recall_score(observado, predicao_binaria, pos_label=0)
+    acuracia = accuracy_score(observado, predicao_binaria)
+
+    # Visualização dos principais indicadores desta matriz de confusão
+    indicadores = pd.DataFrame({'Sensitividade':[sensitividade],
+                                'Especificidade':[especificidade],
+                                'Acurácia':[acuracia]})
+    return indicadores
                                                         
 # In[1]: Importing data:
-data = pd.read_csv('/Users/rodrigomendes/Documents/projects/titanic/train.csv')
+data = pd.read_csv('/Users/rodrigomendes/Documents/projects/titanic_kaggle_competition/train.csv')
 
 data['Survived'] = data['Survived'].astype('category')
 data['Pclass'] = data['Pclass'].astype('category')
@@ -48,18 +78,35 @@ summary_col([model],
 
 # In[5]: 
 data['phat'] = model.predict()
-data['phat_bool'] = 1 - round(data['phat']) 
 data['phat'] = 1 - data['phat']
+indicadores = matriz_confusao(data['phat'],data['Survived'].astype('float'),  0.60)
+data['phat_bool'] = 1 - round(data['phat']) 
 data[['Survived','phat_bool' ,'phat']]
+print(indicadores)
+# In[]
+from sklearn.metrics import roc_curve, auc
 
-data['Survived'] = data['Survived'].astype('float')
-assertivity = data[data['Survived'] == data['phat_bool']]
-accuracy = len(assertivity)/len(data)
-print('Acuracia: ' + str(accuracy))
+# Função 'roc_curve' do pacote 'metrics' do sklearn
+
+fpr, tpr, thresholds =roc_curve(data['Survived'], data['phat'])
+roc_auc = auc(fpr, tpr)
+
+# Plotando a curva ROC
+plt.figure(figsize=(15,10))
+plt.plot(fpr, tpr, marker='o', color='darkorchid', markersize=10, linewidth=3)
+plt.plot(fpr, fpr, color='gray', linestyle='dashed')
+plt.title('Área abaixo da curva: %g' % round(roc_auc, 4) , fontsize=22)
+plt.xlabel('1 - Especificidade', fontsize=20)
+plt.ylabel('Sensitividade', fontsize=20)
+plt.xticks(np.arange(0, 1.1, 0.2), fontsize=14)
+plt.yticks(np.arange(0, 1.1, 0.2), fontsize=14)
+plt.show()
+# In[]
+
 
 # In[6]: Validando acuracia do modelo com dados externos:
     
-data_test = pd.read_csv('/Users/rodrigomendes/Documents/projects/titanic/test.csv')
+data_test = pd.read_csv('/Users/rodrigomendes/Documents/projects/titanic_kaggle_competition/test.csv')
 data_test.info()
 
 # In[6.5]: 
